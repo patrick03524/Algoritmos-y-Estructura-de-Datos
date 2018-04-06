d302f007b30932ca017012cb9276c775f12e57b3
patrick03524 <dibujanteanime024@gmail.com>
2018-04-06 17:46:21 -0500

    Polimorfismo

--- 1° Trabajo/Polimorfismo/main.cpp
+++ 1° Trabajo/Polimorfismo/main.cpp
@@ -1,10 +1,117 @@
 #include <iostream>
-#include "C_padre.h"
-
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
+
+unsigned long inicio,fin,t;
+
+template <class T>
+void swap_p(T &a, T &b);
+
+template <class T>
+void crear_aroi(T *aroi, int num_elem);
+
+template <class T>
+void ord_Cocktail(T *a, int n);
+
+template <class T>
+void printArray(T *a, int n);
+
+template<class T>
+class CPadre{
+public:
+    bool cmp(T a, T b);
+};
+
+template<class T>
+class CHijaMenor {
+public:
+    bool cmp(T a, T b){
+        return a<b;
+    }
+};
+
+template<class T>
+class CHijaMayor {
+public:
+    bool cmp(T a, T b){
+        return a>b;
+    }
+};
+
+template <class T>
+void swap_p(T &a, T &b){
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+template <class T>
+void crear_aroi(T *aroi, int num_elem){ ///solo int's
+    srand(time(NULL));
+    int numero_aleatorio;
+    for(int i=0;i<num_elem;i++){
+        numero_aleatorio=1+rand()%(101-1);  ///numeros entre 1-100
+        *aroi=numero_aleatorio;
+        aroi++;
+    }
+}
+template <class T, class Op>
+void ord_Cocktail(T *a, int n)
+{                           ///ar_start=n-1 & ar_end=0 ascendente
+    Op o;
+    bool comp = true;
+    int ar_start  = 0;          ///0
+    int ar_end = n-1;           ///n-1
+
+    while (comp == true)
+    {
+        comp = false;
+        for (int i = ar_start ; i < ar_end; i++){
+            if (o.cmp(*(a+i), *(a+i+1))){
+                swap_p(*(a+i), *(a+i+1));
+                comp = true;
+            }
+        }
+        if (comp == false){
+                break;
+        }
+        --ar_end;
+        comp = false;
+        for (int i = ar_end - 1; i >= ar_start ; i--){
+            if (o.cmp(*(a+i), *(a+i+1))){
+                swap_p(*(a+i), *(a+i+1));
+                comp = true;
+            }
+        }
+        ++ar_start ;
+    }
+}
+
+template <class T>
+void printArray(T *a, int n)
+{
+    cout << "{ ";
+    for (int i=0; i<n; i++){
+        cout <<*(a+i)<<" ";
+    }
+    cout <<"}"<<endl;
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    const int numero_de_elementos = 500000;
+    int *arr =NULL;
+    arr= new int[numero_de_elementos];
+
+    crear_aroi<int>(arr,numero_de_elementos); ///cantidad de elementos del Array*/
+    inicio=clock();
+    ord_Cocktail<int, CHijaMenor<int> >(arr,numero_de_elementos);
+    fin=clock();
+    double t=(double(fin-inicio)/CLOCKS_PER_SEC);
+	cout << "Tiempo: " << t << " seg" << endl;
+
+    delete[] arr;
+    arr = 0;
     return 0;
 }
